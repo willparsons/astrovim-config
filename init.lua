edba24ee074dbb1df5ea3876cc0373cb5c4f523b
William Parsons-Heins <williamparsons62@gmail.com>
2022-10-19 15:28:43 +0100

    simplify catppuccin config

--- init.lua
+++ init.lua
@@ -57,35 +57,10 @@ local config = {
           require("catppuccin").setup({
             transparent_background = false,
             term_colors = false,
-            dim_inactive = {
-              enabled = false,
-              shade = "dark",
-              percentage = 0.15,
-            },
             styles = {
               comments = {},
               conditionals = {},
-              loops = {},
-              functions = {},
-              keywords = {},
-              strings = {},
-              variables = {},
-              numbers = {},
-              booleans = {},
-              properties = {},
-              types = {},
-              operators = {},
             },
-            integrations = {
-              cmp = true,
-              gitsigns = true,
-              nvimtree = true,
-              telescope = true,
-              treesitter = true,
-              -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-            },
-            color_overrides = {},
-            custom_highlights = {},
           })
         end,
       },
