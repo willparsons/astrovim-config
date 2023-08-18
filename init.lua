196c81eed47dd61e842e49fce42c81971897d9d0
William Parsons-Heins <williamparsons62@gmail.com>
2023-08-18 23:36:43 +0100

    default scheme

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ return {
   updater = {
     branch = "main",
   },
-  colorscheme = "vscode",
+  colorscheme = "onedark",
   polish = function()
     vim.api.nvim_create_augroup("Will", { clear = true })
 
