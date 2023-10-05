638753c6fea063c3b2783f3906292193c0fe893c
William Parsons-Heins <william.parsons-heins@globality.com>
2023-10-05 12:50:46 +0100

    remove idiot typescript pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,6 @@ return {
   { import = "astrocommunity.colorscheme.nord-nvim" },
   { import = "astrocommunity.colorscheme.nvim-juliana" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  { import = "astrocommunity.pack.typescript" },
 
   -- config
   {
