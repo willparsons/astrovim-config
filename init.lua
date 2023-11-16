41a8cbbc05ad137ddcfe316701e27ea65f50d2ed
William Parsons-Heins <williamparsons62@gmail.com>
2023-11-16 21:02:49 +0000

    dont include some options when passing them from tsserver to typescript-tools

--- init.lua
+++ init.lua
@@ -7,14 +7,14 @@ return {
     setup_handlers = {
       -- add custom handler
       tsserver = function(_, opts)
-        -- remove parts of `tsserver` options
-        -- vim.tbl_map(function(k) opts[k] = nil end, {
-        --   "_setup_buffer",
-        --   "cmd",
-        --   "document_config",
-        --   "name",
-        --   "setup",
-        -- })
+        remove parts of `tsserver` options
+        vim.tbl_map(function(k) opts[k] = nil end, {
+          "_setup_buffer",
+          "cmd",
+          "document_config",
+          "name",
+          "setup",
+        })
 
         require("typescript-tools").setup(opts)
       end,
