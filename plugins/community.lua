e20c19e3ef97f9537ce64a605271fe87224c540f
William Parsons-Heins <william.parsons-heins@globality.com>
2023-12-11 11:32:44 +0000

    drop some themes i dont use

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,6 @@ return {
   -- imports
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  { import = "astrocommunity.colorscheme.nord-nvim" },
-  { import = "astrocommunity.colorscheme.nvim-juliana" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.pack.typescript" },
 
