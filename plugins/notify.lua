659b3d8fa7bfb338f741e6ff550c9eee3c4a7c82
William Parsons-Heins <williamparsons62@gmail.com>
2024-01-22 22:45:50 +0000

    configure nvim-notify to be quick and compact

---
+++ plugins/notify.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 500,
+      stages = "fade",
+      render = "wrapped-compact",
+      max_width = 50,
+      max_height = 100,
+    },
+  },
+}
