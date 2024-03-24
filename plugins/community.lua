a7cf6dead1abb5407f51064daae61b823b59b073
William Parsons-Heins <williamparsons62@gmail.com>
2024-03-24 20:40:48 +0000

    update colors

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,14 +1,3 @@
 return {
   "AstroNvim/astrocommunity",
-
-  -- imports
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  -- { import = "astrocommunity.pack.typescript" },
-
-  -- the typescript pack adds this but I'd rather not have it
-  -- {
-  --   "vuki656/package-info.nvim",
-  --   enabled = false,
-  -- },
 }
