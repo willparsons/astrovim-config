d750880102aa7c7d3646b72aabeae03c734f36ab
William Parsons-Heins <williamparsons62@gmail.com>
2024-05-03 20:02:53 +0100

    use gruvbox-material

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -40,4 +40,11 @@ return {
       multiplexer_integration = false,
     },
   },
+
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function() vim.g.gruvbox_material_background = "hard" end,
+  },
 }
