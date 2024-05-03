c47af139355b3f2f590c735b6022f348cf2370b1
William Parsons-Heins <williamparsons62@gmail.com>
2024-05-03 20:03:42 +0100

    use win32yank for wsl

--- lua/polish.lua
+++ lua/polish.lua
@@ -13,3 +13,16 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   command = "%s/\\s\\+$//e",
   group = "Will",
 })
+
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = false,
+}
