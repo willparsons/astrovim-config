4d7a7b3e39131e26d0afef3f8166b43f39bc2b0d
William Parsons-Heins <william.parsons-heins@globality.com>
2024-06-18 17:52:40 +0100

    conform

--- lua/community.lua
+++ lua/community.lua
@@ -2,6 +2,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
 
   {
     "vuki656/package-info.nvim",
