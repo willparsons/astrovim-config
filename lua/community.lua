73face975a6758fe795da0524cd2f81148747d3a
William Parsons-Heins <william.parsons-heins@globality.com>
2024-06-20 13:03:39 +0100

    enable telescope-lsp-mappings

--- lua/community.lua
+++ lua/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 
   {
     "vuki656/package-info.nvim",
