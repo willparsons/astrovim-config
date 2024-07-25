a3ef4551bba1cb6ce7cb14c59446dd6eaa0438e6
William Parsons-Heins <william.parsons-heins@globality.com>
2024-07-25 15:18:48 +0100

    weird prettier shit

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,90 @@
+local function decode_json(filename)
+  -- Open the file in read mode
+  local file = io.open(filename, "r")
+  if not file then
+    return false -- File doesn't exist or cannot be opened
+  end
+
+  -- Read the contents of the file
+  local content = file:read "*all"
+  file:close()
+
+  -- Parse the JSON content
+  local json_parsed, json = pcall(vim.fn.json_decode, content)
+  if not json_parsed or type(json) ~= "table" then
+    return false -- Invalid JSON format
+  end
+  return json
+end
+
+local format_filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+
+local function check_json_key_exists(json, ...) return vim.tbl_get(json, ...) ~= nil end
+local lsp_rooter, prettierrc_rooter
+local has_prettier = function(bufnr)
+  if type(bufnr) ~= "number" then bufnr = vim.api.nvim_get_current_buf() end
+  local rooter = require "astrocore.rooter"
+  if not lsp_rooter then
+    lsp_rooter = rooter.resolve("lsp", {
+      ignore = {
+        servers = function(client)
+          return not vim.tbl_contains({ "vtsls", "typescript-tools", "volar", "eslint", "tsserver" }, client.name)
+        end,
+      },
+    })
+  end
+  if not prettierrc_rooter then
+    prettierrc_rooter = rooter.resolve {
+      ".prettierrc",
+      ".prettierrc.json",
+      ".prettierrc.yml",
+      ".prettierrc.yaml",
+      ".prettierrc.json5",
+      ".prettierrc.js",
+      ".prettierrc.cjs",
+      "prettier.config.js",
+      ".prettierrc.mjs",
+      "prettier.config.mjs",
+      "prettier.config.cjs",
+      ".prettierrc.toml",
+    }
+  end
+  local prettier_dependency = false
+  for _, root in ipairs(require("astrocore").list_insert_unique(lsp_rooter(bufnr), { vim.fn.getcwd() })) do
+    local package_json = decode_json(root .. "/package.json")
+    if
+      package_json
+      and (
+        check_json_key_exists(package_json, "dependencies", "prettier")
+        or check_json_key_exists(package_json, "devDependencies", "prettier")
+      )
+    then
+      prettier_dependency = true
+      break
+    end
+  end
+  return prettier_dependency or next(prettierrc_rooter(bufnr))
+end
+
+local null_ls_formatter = function(params)
+  if vim.tbl_contains(format_filetypes, params.filetype) then return has_prettier(params.bufnr) end
+  return false
+end
+
+return {
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd" })
+      if not opts.handlers then opts.handlers = {} end
+
+      opts.handlers.prettierd = function(source_name, methods)
+        local null_ls = require "null-ls"
+        for _, method in ipairs(methods) do
+          null_ls.register(null_ls.builtins[method][source_name].with { runtime_condition = null_ls_formatter })
+        end
+      end
+    end,
+  },
+}
