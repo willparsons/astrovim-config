b7563fe621390a4a6b4c51de4c1f7c46b96c9112
William Parsons-Heins <william.parsons-heins@globality.com>
2024-08-05 09:31:28 +0100

    add catppuccin

--- lua/community.lua
+++ lua/community.lua
@@ -6,9 +6,16 @@ return {
   { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 
   {
     "vuki656/package-info.nvim",
     enabled = false,
+  },
+
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
   },
 }
