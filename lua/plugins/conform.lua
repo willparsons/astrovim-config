6a9ac5bcaaa10faff5467c4bd6a5443bb64cb0b0
William Parsons-Heins <williamparsons62@gmail.com>
2024-11-21 00:31:37 +0000

    remove conform prettier config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,6 @@
-local util = require "conform.util"
-
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters = {
-      prettierd = {
-        cwd = util.root_file {
-          ".prettierrc",
-          ".prettierrc.json",
-          ".prettierrc.yml",
-          ".prettierrc.yaml",
-          ".prettierrc.json5",
-          ".prettierrc.js",
-          ".prettierrc.cjs",
-          ".prettierrc.mjs",
-          ".prettierrc.toml",
-          "prettier.config.js",
-          "prettier.config.cjs",
-          "prettier.config.mjs",
-        },
-      },
-    },
     log_level = vim.log.levels.DEBUG,
     formatters_by_ft = {
       yaml = { "prettierd" },
