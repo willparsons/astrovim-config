2208381d84a0a9b6e2613e3ba057af8f74ce2a58
William Parsons-Heins <williamparsons62@gmail.com>
2024-11-29 19:38:49 +0000

    stuff

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,6 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
   { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
@@ -18,26 +17,5 @@ return {
   {
     "vuki656/package-info.nvim",
     enabled = false,
-  },
-
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000,
-
-    config = function()
-      require("github-theme").setup {
-        groups = {
-          all = {
-            StatusLine = { fg = "fg2", bg = "bg1" },
-          },
-        },
-      }
-    end,
   },
 }
