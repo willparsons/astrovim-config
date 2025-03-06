ba00dab0b30791afcc5b396aa6fb868d7e0fcd63
William Parsons-Heins <williamparsons62@gmail.com>
2025-03-06 13:27:56 +0000

    stuff

--- lua/community.lua
+++ lua/community.lua
@@ -1,13 +1,11 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.templ" },
   { import = "astrocommunity.pack.gleam" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.editing-support.conform-nvim" },
